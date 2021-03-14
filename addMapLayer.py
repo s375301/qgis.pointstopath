2e7365bb8cea0fd62626731b333642ac8dbd0043
Peter <70194524+s375301@users.noreply.github.com>
2021-03-15 00:53:06 +0900

    Delete addMapLayer.py

--- addMapLayer.py
+++
@@ -1,9 +0,0 @@
-os.chdir('C:/Users/shin/data/sample_shp')
-
-layers = []
-for file in glob.glob('*') :
-    if 'shp' in file.split('.', 2) :
-        slayer = QgsVectorLayer(os.path.basename(file), file.split('.', 2)[0], "ogr")
-        layers.append(slayer)
-
-QgsProject.instance().addMapLayers(layers)
