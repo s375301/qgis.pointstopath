c979645703542e657b41958fbd1686413b45489a
Peter <70194524+s375301@users.noreply.github.com>
2021-03-15 00:30:40 +0900

    file name w/o extention
    split function & glob.glob(*)

--- import_csv_layer.py
+++ import_csv_layer.py
@@ -2,9 +2,9 @@ import os, glob
 os.chdir('/Users/guestname/data')
 
 layers = []
-for file in glob.glob('./*') :
+for file in glob.glob('*') :
     uri = "file:///C:/Users/guestname/data/sample" + file + "?type=csv&maxFields=10000&detectTypes=yes&xField=Longitude&yField=Latitude&crs=EPSG:4326&spatialIndex=no&subsetIndex=no&watchFile=no"
-    vlayer = QgsVectorLayer(uri, os.path.basename(file), "delimitedtext")
+    vlayer = QgsVectorLayer(uri, file.split('.', 2)[0], "delimitedtext")
     layers.append(vlayer)
 #    QgsProject.instance().addMapLayer(vlayer)
 
